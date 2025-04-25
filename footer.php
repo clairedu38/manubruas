8a897b015df89590215fcd1c46b25abed80a4ad1
Claire Duwig <claire.duwig@hotmail.fr>
2025-04-25 11:14:10 +0200

    modifications 1

--- footer.php
+++ footer.php
@@ -7,7 +7,20 @@
             </div>
             <div>
                 <p><?php echo esc_html(get_field('texte_3bis', 2)); ?></p>
-                <div><img src="<?php echo esc_url(get_field('image_3bis', 2)['url']); ?>" alt="Logo 3bis"></div>
+                <?php 
+                $image_3bis = get_field('image_3bis', 2);
+                $lien_3bis = get_field('lien_3bis', 2);
+                if (is_array($image_3bis) && isset($image_3bis['url'])) : ?>
+                    <div class="logo-3bis">
+                        <?php if ($lien_3bis) : ?>
+                            <a href="<?php echo esc_url($lien_3bis); ?>" target="_blank" rel="noopener noreferrer">
+                                <img src="<?php echo esc_url($image_3bis['url']); ?>" alt="Logo 3bis">
+                            </a>
+                        <?php else : ?>
+                            <img src="<?php echo esc_url($image_3bis['url']); ?>" alt="Logo 3bis">
+                        <?php endif; ?>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
 
@@ -17,8 +30,8 @@
                 <?php
                     wp_nav_menu(array(
                         'theme_location' => 'footer1',
-                        'container'      => false, // N'affiche pas le conteneur ul autour du menu
-                        'menu_class'     => 'footer-menu', 
+                        'container'      => false,
+                        'menu_class'     => 'footer-menu',
                     ));
                 ?>
             </div>
@@ -27,8 +40,8 @@
                 <?php
                     wp_nav_menu(array(
                         'theme_location' => 'footer2',
-                        'container'      => false, // N'affiche pas le conteneur ul autour du menu
-                        'menu_class'     => 'footer-menu', 
+                        'container'      => false,
+                        'menu_class'     => 'footer-menu',
                     ));
                 ?>
                 <a class="linkedin-footer" href="<?php echo esc_url(get_field('linkedin', 2)); ?>" target="_blank">
@@ -39,23 +52,35 @@
     </div>
 
     <div class="second-footer">
-  <p>
-    <?php echo esc_html(get_field('copyright', 2)); ?>
-       -    <?php echo esc_html(get_field('nom', 2)); ?>
-       -    <a href="<?php echo esc_url(get_field('mentions_legales', 2)['url']); ?>">
-      <?php echo esc_html(get_field('mentions_legales', 2)['title']); ?>
-    </a>
-       -    <a href="<?php echo esc_url(get_field('rgpd', 2)['url']); ?>">
-      <?php echo esc_html(get_field('rgpd', 2)['title']); ?>
-    </a>
-       -    <a href="<?php echo esc_url(get_field('creation_nom', 2)['url']); ?>">
-      <?php echo esc_html(get_field('creation_nom', 2)['title']); ?>
-    </a>
-  </p>
+    <p>
+        <?php echo esc_html(get_field('copyright', 2)); ?>
+        - <?php echo esc_html(get_field('nom', 2)); ?>
+
+        <?php $mentions = get_field('mentions_legales', 2); ?>
+        <?php if ($mentions && is_array($mentions)) : ?>
+            - <a href="<?php echo esc_url($mentions['url']); ?>">
+                <?php echo esc_html($mentions['title']); ?>
+            </a>
+        <?php endif; ?>
+
+        <?php $rgpd = get_field('rgpd', 2); ?>
+        <?php if ($rgpd && is_array($rgpd)) : ?>
+            - <a href="<?php echo esc_url($rgpd['url']); ?>">
+                <?php echo esc_html($rgpd['title']); ?>
+            </a>
+        <?php endif; ?>
+
+        <?php $creation = get_field('creation_nom', 2); ?>
+        <?php if ($creation && is_array($creation)) : ?>
+            - <a href="<?php echo esc_url($creation['url']); ?>">
+                <?php echo esc_html($creation['title']); ?>
+            </a>
+        <?php endif; ?>
+    </p>
 </div>
 </footer>
 
 <?php wp_footer(); ?>
-  <!-- script Google Analytics ici -->
+<!-- script Google Analytics ici -->
 </body>
 </html>
